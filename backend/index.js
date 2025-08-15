221889fc23ba46afe75a192ef0eb9f5c0847abc7
Philip de la Gueronniere <54122657+crtlnd@users.noreply.github.com>
2025-08-15 17:04:19 -0500

    revert to port 3000

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,4 @@
-const express = require('express'); const app = express(); app.listen(3000, () => console.log('Server running on port 3000'));
-
+const express = require('express');
+const app = express();
+app.get('/api/hello', (req, res) => { res.send('Hello from backend!'); });
+app.listen(3000, () => console.log('Server running on port 3000'));
